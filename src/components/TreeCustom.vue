64987974909b85739c77b41c078bab34be9f9933
jzh <11297332+jiaozh1@user.noreply.gitee.com>
2025-04-15 14:54:57 +0800

    fix: 🐛 drag and drop logic changes

--- src/components/TreeCustom.vue
+++ src/components/TreeCustom.vue
@@ -1,21 +1,25 @@
 <template>
-  <a-tree
-    :show-line="showLine"
-    :show-icon="showIcon"
-    v-model:expandedKeys="expandedKeys"
-    :tree-data="state.gData"
-    draggable
-    @drop="onDrop"
-    @dragover="onDragOver"
-    @dragleave="onDragLeave"
-    :fieldNames="{ children: 'children', title: 'title', key: 'key' }"
-  >
-    <template #title="{ key: treeKey, title }"
-      ><span class="keytag" :idd="treeKey" :title="title">
-        {{ title }}
-      </span>
-    </template>
-  </a-tree>
+  <div class="draggable-tree">
+    <a-tree
+      :show-line="showLine"
+      :show-icon="showIcon"
+      block-node
+      v-model:expandedKeys="expandedKeys"
+      :tree-data="state.gData"
+      draggable
+      @drop="onDrop"
+      @dragstart="onDragStart"
+      @dragover="onDragOver"
+      @dragleave="onDragLeave"
+      :fieldNames="{ children: 'children', title: 'title', key: 'key' }"
+    >
+      <template #title="{ key: treeKey, title }"
+        ><span class="keytag" :idd="treeKey" :title="title">
+          {{ title }}
+        </span>
+      </template>
+    </a-tree>
+  </div>
 
   <div
     v-if="mergePopoverVisible"
@@ -44,8 +48,10 @@ type NodeDragEventParams = {
 
 type TreeDataItem = TreeProps["treeData"][] & {
   children?: Array<TreeDataItem>;
-  key: string;
+  key: keyType;
 };
+
+type keyType = string | number;
 
 const props = defineProps({
   gData: {
@@ -72,15 +78,86 @@ const showIcon = ref<boolean>(false);
 
 const expandedKeys = ref<string[]>(["0-0", "0-1"]);
 
-const dropNodeKey = ref("");
+const dragNode = ref<DataNode>();
+const dropNodeKey = ref<keyType>("");
+
+/**
+ * 拖拽开始的时候记录拖动的节点
+ * @param info 拖拽源节点Info
+ */
+const onDragStart = (info: NodeDragEventParams) => {
+  dragNode.value = info.node;
+};
+
+//处理 Hover到节点之上的提示效果,因使用的Event的参数，并不能跟准确的get到要使用的信息，所以使用了Dom
+const onDragOver = (info: NodeDragEventParams) => {
+  let ele = document.getElementsByClassName(
+    "ant-tree-drop-indicator"
+  )[0] as HTMLElement;
+  let rect: DOMRect;
+  if (ele) {
+    rect = ele.getBoundingClientRect();
+    dropNodeTitle.value =
+      ele.parentElement
+        ?.querySelector('[class="keytag"]')
+        ?.getAttribute("title") || "";
+
+    dropNodeKey.value =
+      ele.parentElement
+        ?.querySelector('[class="keytag"]')
+        ?.getAttribute("idd") || "";
+
+    const dropNode = findNode(state.gData, dropNodeKey.value);
+
+    let diffLevel = 0;
+    diffLevel = dropNode.level - dragNode?.value?.level;
+    let pos = parseInt(ele.style.top) < 0 ? "之上" : "之下";
+    console.log(diffLevel);
+    if (diffLevel === 0) {
+      popMsg.value =
+        parseInt(ele.style.left) === 28
+          ? `将与【${dropNodeTitle.value}】合并`
+          : `将置于【${dropNodeTitle.value}】${pos}`;
+    } else if (diffLevel === 1) {
+      popMsg.value =
+        parseInt(ele.style.left) === 28
+          ? `将合入【${dropNodeTitle.value}】的子级`
+          : `不可跨级拖拽`;
+    } else {
+      popMsg.value = `不可跨级拖拽`;
+    }
+
+    if (pos) {
+      popoverStyle.value = {
+        position: "fixed",
+        left: `${rect.left + 40}px`,
+        top: `${rect.top + 30}px`,
+        zIndex: 9999,
+      };
+      mergePopoverVisible.value = true;
+    } else {
+      mergePopoverVisible.value = false;
+    }
+  } else {
+    mergePopoverVisible.value = false;
+  }
+};
+
+/**
+ * 鼠标离开，移除提示框
+ */
+const onDragLeave = () => {
+  mergePopoverVisible.value = false;
+  dropNodeKey.value = "";
+};
 
 //移动到最终节点
 const onDrop = (info: AntTreeNodeDropEvent) => {
   mergePopoverVisible.value = false;
   let node: DataNode = info.node;
-  if (dropNodeKey.value && dropNodeKey.value !== node.key) {
-    node = findNode(state.gData, dropNodeKey.value);
-  }
+  // if (dropNodeKey.value && dropNodeKey.value !== node.key) {
+  //   node = findNode(state.gData, dropNodeKey.value);
+  // }
 
   const dropKey = node.key;
   const dragKey = info.dragNode.key;
@@ -91,7 +168,8 @@ const onDrop = (info: AntTreeNodeDropEvent) => {
   const dragParent = findParent(state.gData, dragKey);
   const dropParent = findParent(state.gData, dropKey);
 
-  if (dragParent?.level !== dropParent?.level) {
+  const diffLevel = node.level - dragNode?.value?.level;
+  if (!(diffLevel === 0 || diffLevel === 1)) {
     // 如果不在同一层级，则不执行拖拽操作或给出提示
     message.error("不能跨层级拖拽");
     return; // 阻止默认行为，例如阻止重新排序或移动节点到其他父节点下
@@ -126,7 +204,15 @@ const onDrop = (info: AntTreeNodeDropEvent) => {
   );
   if (!info.dropToGap) {
     // Drop on the content
-    mergeNodes(dragObj, dropKey);
+    if (diffLevel == 0) {
+      mergeNodes(dragObj, dropKey);
+    } else if (diffLevel == 1) {
+      loop(data, dropKey, (item: TreeDataItem) => {
+        item.children = item.children || [];
+        // where to insert 示例添加到头部，可以是随意位置
+        item.children.unshift(dragObj);
+      });
+    }
   } else if (
     (node.children || []).length > 0 && // Has children
     node.expanded && // Is expanded
@@ -157,51 +243,11 @@ const onDrop = (info: AntTreeNodeDropEvent) => {
   state.gData = data;
 };
 
-//处理 Hover到节点之上的提示效果,因使用的Event的参数，并不能跟准确的get到要使用的信息，所以使用了Dom
-const onDragOver = (info: NodeDragEventParams) => {
-  let ele = document.getElementsByClassName(
-    "ant-tree-drop-indicator"
-  )[0] as HTMLElement;
-  let rect: DOMRect;
-  if (ele) {
-    rect = ele.getBoundingClientRect();
-    dropNodeTitle.value =
-      ele.parentElement
-        ?.querySelector('[class="keytag"]')
-        ?.getAttribute("title") || "";
-
-    dropNodeKey.value =
-      ele.parentElement
-        ?.querySelector('[class="keytag"]')
-        ?.getAttribute("idd") || "";
-    Pos.value = parseInt(ele.style.top) < 0 ? "之上" : "之下";
-    popMsg.value =
-      parseInt(ele.style.left) === 28
-        ? `将与【${dropNodeTitle.value}】合并`
-        : `将置于【${dropNodeTitle.value}】${Pos.value}`;
-    if (Pos.value) {
-      popoverStyle.value = {
-        position: "fixed",
-        left: `${rect.left + 10}px`,
-        top: `${rect.top}px`,
-        zIndex: 9999,
-      };
-      mergePopoverVisible.value = true;
-    } else {
-      mergePopoverVisible.value = false;
-    }
-  } else {
-    mergePopoverVisible.value = false;
-  }
-};
-
-//鼠标离开，移除提示框
-const onDragLeave = () => {
-  mergePopoverVisible.value = false;
-  dropNodeKey.value = "";
-};
-
-//处理合并节点的逻辑函数
+/**
+ *处理合并节点的逻辑函数
+ * @param sourceNode 拖拽的源节点
+ * @param targetKey  拖拽的目标节点
+ */
 const mergeNodes = (sourceNode: DataNode, targetKey: string | number) => {
   const data = [...(state.gData ? state.gData : [])];
   //const sourceNode = findNode(data, sourceKey);
@@ -275,6 +321,10 @@ const removeNode = (tree: any, key: any) => {
 };
 </script>
 <style lang="less" scoped>
+.draggable-tree {
+  width: 300px;
+  text-align: left;
+}
 .pop-div {
   background-color: #1677ff;
   padding: 6px 10px;
